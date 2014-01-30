82a6dbe4bf23cac82583c4eecd5d336ff7e27e8b
JordanNg <jong@iolani.org>
2014-01-29 20:26:46 -1000

    added exercises 1 & 2
    Meow

---
+++ ex1.rb
@@ -0,0 +1,13 @@
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts 'Yay! Printing.'
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+
+#Made it print 'MEOOOOWWWWWW'
+#Going to comment out everything in order to print only one line
+#Putting in the (octothorpe) makes the line of code commented out
+
+puts "MEOOOOWWWWWW"
